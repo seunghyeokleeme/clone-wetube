6b8f751681c2f1b84c9d2c6df61eab6cb8cf8e32
Seunghyeok Lee <seunghyeok.tech@gmail.com>
2024-06-13 00:39:36 +0900

    3.2 GET Requests part Two (09:42)

--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
 import express from "express";
 
 const PORT = 4000;
-
 const app = express();
+
+const handleHome = (req, res) => {
+  console.log(req);
+};
+
+app.get("/", handleHome);
 
 const handleListening = () =>
   console.log(`✅ Server listening on http://localhost:${PORT}`);
