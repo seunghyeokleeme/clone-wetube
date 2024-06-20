43666894074a81f6e13f61469ef71d281c879816
Seunghyeok Lee <seunghyeok.tech@gmail.com>
2024-06-21 01:40:59 +0900

    response

--- src/server.js
+++ src/server.js
@@ -3,9 +3,17 @@ import express from 'express';
 const PORT = 4000;
 const app = express();
 
-const handleHome = (req, res) => console.log(req);
+const getHome = (req, res) => {
+  return res.send('Home!');
+};
 
-app.get('/', handleHome);
+app.get('/', getHome);
+
+const getLogin = (req, res) => {
+  return res.send('login');
+};
+
+app.get('/login', getLogin);
 
 const handleListening = () =>
   console.log(`✅ Server listening on http://localhost:${PORT}`);
