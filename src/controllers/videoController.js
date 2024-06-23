f541a60d8d1093729d43c492c9146982667fdb7a
Seunghyeok Lee <seunghyeok.tech@gmail.com>
2024-06-23 18:37:42 +0900

    5.3 Extending Templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,11 +2,11 @@ export const trending = (req, res) => res.render('home');
 
 export const see = (req, res) => res.render('watch');
 
-export const upload = (req, res) => res.render('edit');
-
-export const edit = (req, res) => {
-  return res.send('Edit Video');
+export const upload = (req, res) => {
+  return res.send('Upload Video');
 }
+
+export const edit = (req, res) => res.render('edit');
 
 export const remove = (req, res) => {
   return res.send('Remove Video');
