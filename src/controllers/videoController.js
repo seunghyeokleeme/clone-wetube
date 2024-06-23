a676b68b9dac00f6c776360ef0baeb93c5290e0b
Seunghyeok Lee <seunghyeok.tech@gmail.com>
2024-06-23 19:06:03 +0900

    Variables to Templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,15 +1,15 @@
-export const trending = (req, res) => res.render('home');
+export const trending = (req, res) => res.render('home', { pageTitle: 'Home' });
 
-export const see = (req, res) => res.render('watch');
+export const see = (req, res) => res.render('watch', { pageTitle: req.params.id });
 
 export const upload = (req, res) => {
   return res.send('Upload Video');
-}
+};
 
-export const edit = (req, res) => res.render('edit');
+export const edit = (req, res) => res.render('edit', { pageTitle: 'Edit'});
 
 export const remove = (req, res) => {
   return res.send('Remove Video');
-}
+};
 
 export const search = (req, res) => res.send('Search');
